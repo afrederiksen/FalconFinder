52e3c4b66a9dcae6b38f8e64993ba271b481d59a
Adam Frederiksen <26208927+afrederiksen@users.noreply.github.com>
2020-10-15 14:40:40 -0400

    Added basic methods to Admin and Leasee
    Needs to refine the addListing method

--- src/Admin.java
+++ src/Admin.java
@@ -1,4 +1,35 @@
 
-public class Admin extends User{
+public class Admin extends User {
+	private void postListing() {
+
+	}
+
+	private void removeListing(String title) {
+
+	}
+
+	private void getDistanceFromCampus() {
+
+	}
+
+	private void removeUser(String UUID) {
+
+	}
+
+	private void addUser(String UUID, String email, String password, String type) {
+
+	}
+
+	private void suspendUser(String UUID) {
+
+	}
+
+	private void purgeLease(String leaseUUID) {
+
+	}
+
+	private void getLease(String leaseUUID) {
+
+	}
 
 }
--- src/Leasee.java
+++ src/Leasee.java
@@ -1,4 +1,39 @@
+import java.util.ArrayList;
 
 public class Leasee extends User {
+	ArrayList<Lease> leases = new ArrayList<Lease>();
+	ArrayList<String> favoriteProperties = new ArrayList<String>();
+	String USCID = "";
 
+	private boolean isValidUSCID(String USCID) {
+		return true;
+	}
+
+	private String getClassStanding(String USCID) {
+		return "";
+	}
+
+	private void contactLandlord(String landlord, String message) {
+	}
+
+	private void signLease(boolean signed, String date) {
+		// java.time.LocalDateTime.now()
+	}
+
+	private void postSublease() {
+		// Parameters must be updated...
+	}
+
+	private void removeListing(String listingID) {
+	}
+
+	private void reviewListing(String listingID, String review, int rating) {
+	}
+
+	private void addFavoriteProperty(String ListingID) {
+
+	}
+
+	private void deleteFavoriteProperty(String listindID) {
+	}
 }
