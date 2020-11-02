e048c5df02cadca5cbf7eacd16a8809a71b0e829
Nicholasgarofalo <70486605+Nicholasgarofalo@users.noreply.github.com>
2020-11-01 19:01:46 -0500

    Update Landlords.java

--- src/Landlords.java
+++ src/Landlords.java
@@ -4,10 +4,14 @@ public class Landlords {
   private static Landlords landlords = null;
   private static ArrayList<Landlord> landlordList = new ArrayList<Landlord>();
 
+
   private Landlords() {
     landlordList = DataReader.loadLandlords();
   }
 
+  /**
+   *returns instance of Landlords
+  */
   public static Landlords getInstance() {
     if (landlords == null) {
       landlords = new Landlords();
@@ -16,10 +20,16 @@ public class Landlords {
     return landlords;
   }
 
+  /**
+   *returns the arrayList of type Landlord
+  */
   public ArrayList<Landlord> getLandlords() {
     return landlordList;
   }
 
+  /**
+   *adds type Landlord to landlordList
+  */
   public static void addLandlord(String id, String firstName, String lastName, String address,
       String email, String password, String type, ArrayList<Listing> listings) {
     landlordList
