2dff82e3fc1f6eb53203b6a4f8b78f23b48d759d
Adam Frederiksen <26208927+afrederiksen@users.noreply.github.com>
2020-11-14 19:04:46 -0500

    Update ListingTest.java
    Needs to be tested and setup in Eclipse.

--- src/ListingTest.java
+++ src/ListingTest.java
@@ -1,9 +1,18 @@
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.*;
+import java.util.ArrayList;
 
-public class ListingTest {
-/*(long listingId, String address, long beds, long bathrooms, double distanceFromRussellHouse,
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ListingTest {
+	/*(long listingId, String address, long beds, long bathrooms, double distanceFromRussellHouse,
 	double price, long available, String landlordId, boolean amenitiesWasher, boolean amenitiesAC,
 	boolean amenitiesFurniture, boolean amenitiesPatio, boolean amenitiesDishwasher,
 	boolean allowsPets, boolean amenitiesWifi, boolean amenitiesPool)*/
@@ -12,8 +21,9 @@ public class ListingTest {
 
 	@BeforeClass
 	public static void oneTimeSetup() {
-		listings.addListing(0, "123 Baker Street", 0, 0, 0, 0, -400, null, false, false, false, false, false, false, false, false);
-		assertEquals(6, listings.getListings().size());
+		int size = listings.getListings().size();
+		listings.addListing(42, "123 Baker Street", 0, 0, 0, 0, -400, null, false, false, false, false, false, false, false, false);
+		assertEquals((size+1), listings.getListings().size());
 	}
 	@AfterClass
 	public static void onTimeTearDown() {
@@ -43,10 +53,66 @@ public class ListingTest {
 	public void streetIsValid() {
 		assertNotNull(listings.getListings().get(0).getAddress());
 	}
+//	@BeforeAll
+//	static void setUpBeforeClass() throws Exception {
+//		
+//	}
+//
+//	@AfterAll
+//	static void tearDownAfterClass() throws Exception {
+//	}
+//
+//	@BeforeEach
+//	void setUp() throws Exception {
+//	}
+//
+//	@AfterEach
+//	void tearDown() throws Exception {
+//	}
+//
+//	@Test
+//	void testListing() {
+//		fail("Not yet implemented");
+//	}
+
+	@Test
+	void testAddReview() {
+		listings.getListings().get(1).addReview("Test");
+		assertTrue(listings.getListings().get(1).getReviewList().get(listings.getListings().get(1).getReviewList().size()).contentEquals("Test"));
+	}
+
+	@Test
+	void testAddRating() {
+		listings.getListings().get(1).addRating(5);
+		double orginRating = listings.getListings().get(1).getRating();
+		listings.getListings().get(1).addRating(1);
+		listings.getListings().get(1).addRating(1);
+		assertFalse(listings.getListings().get(1).getRating() == orginRating);
+	}
+	@Test
+	void TestMassRating() {
+		listings.getListings().get(2).addRating(1000000);
+		assertFalse(listings.getListings().get(2).getRating()>5);
+	}
 	
-/* allFalse
- * allTrue
- * 
- * 
- */
+	@Test
+	void duplicateListingID() {
+		//Same ID of 20
+		listings.addListing(20, "2020 Long Drive", 0, 0, 0, 0, -400, null, false, false, false, false, false, false, false, false);
+		listings.addListing(20, "2021 Main Street", 0, 0, 0, 0, -400, null, false, false, false, false, false, false, false, false);
+		ArrayList<Long> idHolding = new ArrayList<Long>();
+		boolean flag = false;
+		for(Listing listing : listings.getListings()) {
+			idHolding.add(listing.getListingId());
+			for(long item : idHolding) {
+				if(listing.getListingId() == item) {
+					flag = true;
+				}
+			}
+			if(flag) {
+				break;
+			}
+		}
+		assertFalse(flag);
+	}
 }
