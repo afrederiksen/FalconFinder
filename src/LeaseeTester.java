cbf0e4e0e9e2e529a23be3ab3156a7393c209ff5
Nicholasgarofalo <70486605+Nicholasgarofalo@users.noreply.github.com>
2020-11-15 23:25:24 -0500

    Update LeaseeTester.java

--- src/LeaseeTester.java
+++ src/LeaseeTester.java
@@ -45,14 +45,14 @@ class LeaseeTester {
 	
 	@Test
 	public void testAddFavoriteProperty() {
-		Listing listingID = new Listing(0, null, 0, 0, 0, 0, 0, null, false, false, false, false, false, false, false, false);
+		Listing listingID = new Listing(12, "123 Baker Street", 0, 0, 0, 0, -400, null, false, false, false, false, false, false, false, false);
 		leasee.addFavoriteProperty(listingID);
 		assertEquals(leasee.getFavoriteListings(), listingID);
 	}
 	
 	@Test
 	public void testDeleteFavoriteProperty() {
-		Listing listingID = new Listing(0, null, 0, 0, 0, 0, 0, null, false, false, false, false, false, false, false, false);
+		Listing listingID = new Listing(12, "123 Baker Street", 0, 0, 0, 0, -400, null, false, false, false, false, false, false, false, false);
 		leasee.deleteFavoriteProperty(listingID);
 		assertNotEquals(leasee.getFavoriteListings(), listingID);
 	}
